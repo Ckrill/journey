7f71618916146b995f0d3f6ade4a5661bdd98b54
Chris Toft Christensen <chris.christensen@valtech.com>
2017-09-07 21:48:45 +0200

    Initial push

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+// Functions
+
+// Debounce
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+$(document).ready(function(){
+    
+    // If scroll
+//    $(document).on('scroll', debounce(function () {
+//        // Do something
+//    }, 50));
+});
